35f600598d8784b157bf21607b9b0d8789b55883
cmtp <christian.tola@hotmail.com>
2019-03-21 14:14:29 -0400

    Add yargs into cli app

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,25 +1,26 @@
+const argv = require('yargs')
+                .command('list', 'Print in cli the table', {
+                    base: {
+                        demand: true,
+                        alias: 'b'
+                    },
+                    limit: {
+                        alias: 'l',
+                        default: 10
+                    }
+                })
+                .help()
+                .argv;
+
 const { createFile } = require('./multiply/multiply');
 
-// let base = 'abc';
-// let data = '';
+console.log(argv.limit);
 
-// for (let i = 1; i <= 10; i++) {
-//     data += `${base} * ${i} = ${base*i}\n`;
-// }
-
-// fs.writeFile(`tables/table-${base}.txt`, data, (err) => {
-//     if(err) throw err;
-//     console.log('The file has been sabed');
-// });
-
-// console.log(process.argv);
-
-let argv = process.argv;
-let param = argv[2];
-let base = param.split('=')[1];
+// let param = argv[2];
+// let base = param.split('=')[1];
 
 
 
-createFile(base)
-    .then(file => console.log(`File created: ${file}`))
-    .catch(e => console.log(e));
+// createFile(base)
+//     .then(file => console.log(`File created: ${file}`))
+//     .catch(e => console.log(e));
