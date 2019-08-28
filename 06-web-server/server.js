568e128431a2ce7d4cf046c99651c44636a6b74e
cmtp <christian.tola@hotmail.com>
2019-08-28 16:26:14 -0400

    Add helpers

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const hbs = require("hbs");
 
+require('./hbs/helpers');
+
 const app = express();
 const port = 3000;
 
@@ -8,19 +10,18 @@ app.use(express.static(__dirname + "/public"));
 
 hbs.registerPartials(__dirname + "/views/partials");
 
+
+
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.render("home", {
-    name: "Christian",
-    year: new Date().getFullYear()
+    name: "Christian"
   });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {
-    year: new Date().getFullYear()
-  });
+  res.render("about");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
