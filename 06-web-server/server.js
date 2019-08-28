733aeefaa641cab586cf5c4890258b3bbd534a59
cmtp <christian.tola@hotmail.com>
2019-08-28 14:28:59 -0400

    add express server

---
+++ 06-web-server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    let result = {
+        name: 'Chris',
+        age: '28',
+        url: req.url
+    }
+
+    res.send(result);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
