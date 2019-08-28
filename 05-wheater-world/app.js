e2eda85062cb268c878125d4878725016b0adb07
cmtp <christian.tola@hotmail.com>
2019-08-28 11:15:18 -0400

    add test with axios

--- 05-wheater-world/app.js
+++ 05-wheater-world/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     address: {
         alias: 'a',
@@ -7,3 +9,16 @@ const argv = require('yargs').options({
 }).argv;
 
 console.log(argv);
+
+let encodedUrl = encodeURI(argv.address);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodedUrl }&key=AIzaSyA_uVKWprjIgPURNhl1v9zzTPLQJIBdi6I`)
+    .then(res => {
+        let location = res.data.results[0];
+        let position = location.geometry.location;
+
+        console.log('ADDRESS: ', location.formatted_address);
+        console.log('LNG: ', position.lat);
+        console.log('LAT: ', position.lat);
+    })
+    .catch( e => console.log('ERROR!!!: ', e))
