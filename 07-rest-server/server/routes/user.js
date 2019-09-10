645c075ae3748710b931112458a59ae44973ecbf
cmtp <christian.tola@hotmail.com>
2019-09-10 18:21:11 -0400

    add skip and limits

--- 07-rest-server/server/routes/user.js
+++ 07-rest-server/server/routes/user.js
@@ -6,7 +6,26 @@ const _ = require("lodash");
 const app = express();
 
 app.get("/user", (req, res) => {
-  res.json("get User");
+  let from = req.query.from || 0;
+  from = Number(from);
+  let limit = req.query.limit || 5;
+  limit = Number(limit);
+
+  User.find({})
+  .skip(from)
+  .limit(limit)
+    .exec( (err, users) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          message: err
+        });
+      }
+      res.json({
+        ok: true,
+        users
+      });
+    })
 });
 
 app.post("/user", (req, res) => {
