d1aaae29a56f7a4cb828dd8f86fb205055d4f582
cmtp <christian.tola@hotmail.com>
2019-09-17 16:38:34 -0400

    Add middlewares in all services

--- 07-rest-server/server/config/config.js
+++ 07-rest-server/server/config/config.js
@@ -1,4 +1,6 @@
-
-
 process.env.PORT = process.env.PORT || 3000;
 
+// 60 seg * 60 min * 24 hrs * 30 days
+process.env.TIME_TOKEN = 60 * 60 * 24 * 30;
+
+process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
